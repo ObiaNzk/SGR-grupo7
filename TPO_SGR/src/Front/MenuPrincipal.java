c24adc5baa017a5709a8738fad3cd9044e56e2f6
Marco <67433887+marcogarciadev@users.noreply.github.com>
2020-11-11 21:09:29 -0300

    Nueva Carga Front.

--- TPO_SGR/src/Front/MenuPrincipal.java
+++ TPO_SGR/src/Front/MenuPrincipal.java
@@ -5,16 +5,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class OperatoriaCheque extends JFrame{
+public class MenuPrincipal extends JFrame{
     private JPanel pnlPrincipal;
     private JPanel pnlMenu;
     private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private OperatoriaCheque self;
+    private JButton consultasSGRButton;
+    private JButton test2Button;
+    private JButton test3Button;
+    private MenuPrincipal self;
 
-    public OperatoriaCheque(String titulo) {
+    public MenuPrincipal(String titulo) {
         super(titulo);
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -48,7 +48,7 @@ public class OperatoriaCheque extends JFrame{
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                CargarDatosCheque frame = new CargarDatosCheque(self, "Ingrese los datos del cheque. ");
+                OperacionesDisponibles frame = new OperacionesDisponibles(self, "Ingrese los datos del cheque. ");
                 frame.setVisible(true);
 
             }
@@ -57,7 +57,7 @@ public class OperatoriaCheque extends JFrame{
 
     public static void main(String[] args)
     {
-        OperatoriaCheque frame = new OperatoriaCheque("Pantalla de Carga Operacion Test");
+        MenuPrincipal frame = new MenuPrincipal("Pantalla de Carga Operacion Test");
 
         frame.setVisible(true);
 
--- TPO_SGR/src/Front/OperacionesDisponibles.java
+++ TPO_SGR/src/Front/OperacionesDisponibles.java
@@ -3,26 +3,36 @@ package Front;
 import javax.swing.*;
 import java.awt.*;
 //Extiende de Jdialog para utilizar los métodos que le dan vida la interfaz.
-public class CargarDatosCheque extends JDialog {
+public class OperacionesDisponibles extends JDialog {
 
     private JPanel pnlPrincipal;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JDesktopPane DesktopPaneSolapaUno;
 
-    public CargarDatosCheque(Window owner, String titulo) {
+    public OperacionesDisponibles(Window owner, String titulo) {
         //Define un owner que gestiona su lanzamiento, (panel principal, clase Operatoria Cheque.
         super(owner, titulo);
 
         //tamaño del panel.
-        this.setSize(300,300);
+        this.setSize(300, 300);
 
         this.setContentPane(pnlPrincipal);
         //No permite volver hasta la pantalla anterior hasta cerrar esta.
+
+        //Inicio Centrado
+        this.setLocationRelativeTo(null);
+
         this.setModal(true);
 
         //Comportamiento de Cierre
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        //Inicio Centrado
-        this.setLocationRelativeTo(null);
+        SolapaInterna solapaframe = new SolapaInterna("Solapa Embebida");
+        solapaframe.setVisible(true);
+        DesktopPaneSolapaUno.add(solapaframe);
+
+
 
 
     }
