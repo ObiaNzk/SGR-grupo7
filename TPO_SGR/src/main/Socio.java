7b734c247054a9f24d086f6be1a32fa69dc66816
matiescobar <matiasjoel.e@gmail.com>
2020-11-11 21:58:33 -0300

    Agrego logica solicitudGarantia

--- TPO_SGR/src/main/Socio.java
+++ TPO_SGR/src/main/Socio.java
@@ -1,9 +1,13 @@
 package main;
 
+import documentaciones.DocumentosOperacion;
+import dtos.LineaDeCreditoDTO;
 import enums.TipoDeOperacionEnum;
 import enums.TipoDeSocio;
+import operaciones.Cheque;
 import operaciones.Comision;
 import operaciones.ContraGarantia;
+import operaciones.LineaDeCredito;
 
 import java.util.Date;
 import java.util.List;
@@ -12,7 +16,7 @@ public class Socio {
 
     private TipoDeSocio tipoDeSocio;
     private List<ContraGarantia> contraGarantiaList;
-    private Integer lineaDeCredito;
+    private LineaDeCredito lineaDeCredito;
     private List<Comision> comisionesList;
     private List<Operacion> operacionList;
     private List<String> facturasList;
@@ -22,10 +26,6 @@ public class Socio {
     private Date vigenciaLineaCredito;
     private Empresa empresa;
 
-
-    public Integer getLineaDeCredito() {
-        return lineaDeCredito;
-    }
 
     public List<Operacion> getOperacionList() {
         return operacionList;
@@ -46,4 +46,88 @@ public class Socio {
     public Empresa getEmpresa() {
         return empresa;
     }
+
+    public LineaDeCredito getLineaDeCredito() {
+        return lineaDeCredito;
+    }
+
+
+    public void setLineaDeCredito(LineaDeCredito lineaDeCredito) {
+        this.lineaDeCredito = lineaDeCredito;
+    }
+
+    public boolean lineaDeCreditoIsVigente(){
+        Date date = new Date();
+            if(date.after(this.lineaDeCredito.getFechaVencimiento())) {
+                return true;
+            }
+            return false;
+    }
+
+    public boolean excedeMaximoFDR(Integer FDR, Operacion operacion){
+
+        double porcentajeOperacion = operacion.getMonto() * 0.05;
+
+        if( FDR > porcentajeOperacion ){
+            return true;
+        }
+        return false;
+    }
+
+
+    public LineaDeCreditoDTO solicitarGarantia(DocumentosOperacion documentosOperacion){
+
+        LineaDeCreditoDTO responseDTO = new LineaDeCreditoDTO();
+
+        if(lineaDeCreditoIsVigente()) {
+            return this.validateDocumentacion(documentosOperacion);
+
+        } else {
+            responseDTO.setError(Boolean.TRUE);
+            responseDTO.setErrorMesagge("Linea de credito vencida");
+            return responseDTO;
+        }
+
+    }
+
+
+    public LineaDeCreditoDTO validateDocumentacion(DocumentosOperacion documentosOperacion) {
+
+        LineaDeCreditoDTO responseDTO = new LineaDeCreditoDTO();
+
+            if (TipoDeOperacionEnum.TIPO1.equals(documentosOperacion.getTipoDeOperacion())) {
+                if (documentosOperacion.getCheque().validateDocumentacionCompleta()) {
+                    responseDTO.setError(Boolean.FALSE);
+                    responseDTO.setErrorMesagge("Solicitud de contragarantia exitosa");
+                    return responseDTO;
+                }
+
+                responseDTO.setError(Boolean.TRUE);
+                responseDTO.setErrorMesagge("Por favor verifique la documentación faltante");
+
+            } else if (TipoDeOperacionEnum.TIPO2.equals(documentosOperacion.getTipoDeOperacion())) {
+                if (documentosOperacion.getCuentaCorriente().validateDocumentacionCompleta()) {
+                    responseDTO.setError(Boolean.FALSE);
+                    responseDTO.setErrorMesagge("Solicitud de contragarantia exitosa");
+                    return responseDTO;
+                }
+
+                responseDTO.setError(Boolean.TRUE);
+                responseDTO.setErrorMesagge("Por favor verifique la documentación faltante");
+
+            } else {
+                if (documentosOperacion.getPrestamo().validateDocumentacionCompleta()) {
+                    responseDTO.setError(Boolean.FALSE);
+                    responseDTO.setErrorMesagge("Solicitud de contragarantia exitosa");
+                    return responseDTO;
+                }
+
+                responseDTO.setError(Boolean.TRUE);
+                responseDTO.setErrorMesagge("Por favor verifique la documentación faltante");
+            }
+
+        return responseDTO;
+    }
+
+
 }
--- TPO_SGR/src/operaciones/Prestamo.java
+++ TPO_SGR/src/operaciones/Prestamo.java
@@ -13,27 +13,69 @@ public class Prestamo {
     private List<Cuota> cuotas;
     private SistemaPrestamoEnum sistema;
 
+    public Prestamo(String banco, Integer monto, Integer tasa, Date fechaAcreditacion, List<Cuota> cuotas, SistemaPrestamoEnum sistema) {
+        Banco = banco;
+        Monto = monto;
+        this.tasa = tasa;
+        FechaAcreditacion = fechaAcreditacion;
+        this.cuotas = cuotas;
+        this.sistema = sistema;
+    }
+
+    public boolean validateDocumentacionCompleta(){
+        if((this.getBanco() != null && !this.getBanco().isEmpty()) && (this.getMonto() != null) &&
+                (this.getTasa() != null) && (this.getFechaAcreditacion() != null) && (!this.getCuotas().isEmpty())
+                && (this.getSistema() != null)){
+            return true;
+        }
+        return false;
+    }
+
     public String getBanco() {
         return Banco;
+    }
+
+    public void setBanco(String banco) {
+        Banco = banco;
     }
 
     public Integer getMonto() {
         return Monto;
     }
 
+    public void setMonto(Integer monto) {
+        Monto = monto;
+    }
+
     public Integer getTasa() {
         return tasa;
+    }
+
+    public void setTasa(Integer tasa) {
+        this.tasa = tasa;
     }
 
     public Date getFechaAcreditacion() {
         return FechaAcreditacion;
     }
 
+    public void setFechaAcreditacion(Date fechaAcreditacion) {
+        FechaAcreditacion = fechaAcreditacion;
+    }
+
     public List<Cuota> getCuotas() {
         return cuotas;
     }
 
+    public void setCuotas(List<Cuota> cuotas) {
+        this.cuotas = cuotas;
+    }
+
     public SistemaPrestamoEnum getSistema() {
         return sistema;
+    }
+
+    public void setSistema(SistemaPrestamoEnum sistema) {
+        this.sistema = sistema;
     }
 }
